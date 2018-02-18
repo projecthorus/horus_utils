aaf8fe9222a2f72f6f45ff9db14feab5b5335aa1
Mark Jessop <vk5qi@rfhead.net>
2018-02-18 17:06:17 +1030

    Add OziPlotter packet listener object to horuslib.listener

--- horuslib/listener.py
+++ horuslib/listener.py
@@ -8,6 +8,8 @@
 
 import socket, json, sys, traceback
 from threading import Thread
+from dateutil.parser import parse
+from datetime import datetime
 from . import *
 from .packets import *
 
@@ -90,6 +92,156 @@ class UDPListener(object):
     def close(self):
         self.udp_listener_running = False
         self.listener_thread.join()
+
+
+class OziListener(object):
+    """
+    Listen on a supplied UDP port for OziPlotter-compatible telemetry data.
+
+    Incoming sentences are of the form:
+    TELEMETRY.HH:MM:SS,latitude,longitude,altitude\n
+    WAYPOINT,waypoint_name,latitude,longitude,comment\n
+    """
+
+    allowed_sentences = ['TELEMETRY', 'WAYPOINT']
+
+    def __init__(self,
+                hostname = '',
+                port = 8942,
+                telemetry_callback = None,
+                waypoint_callback = None):
+
+        self.input_host = hostname
+        self.input_port = port
+        self.telemetry_callback = telemetry_callback
+        self.waypoint_callback = waypoint_callback
+
+        self.start()
+
+
+    def start(self):
+        ''' Start the UDP Listener Thread. '''
+        self.udp_listener_running = True
+
+        self.t = Thread(target=self.udp_rx_thread)
+        self.t.start()
+
+
+    def udp_rx_thread(self):
+        """
+        Listen for incoming UDP packets, and pass them off to another function to be processed.
+        """
+
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.s.settimeout(1)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        except:
+            pass
+        self.s.bind((self.input_host, self.input_port))
+        
+        while self.udp_listener_running:
+            try:
+                m = self.s.recvfrom(256)
+            except socket.timeout:
+                m = None
+            
+            if m != None:
+                try:
+                    self.handle_packet(m[0])
+                except:
+                    traceback.print_exc()
+                    print("ERROR: Couldn't handle packet correctly.")
+                    pass
+        
+        print("INFO: Closing UDP Listener Thread")
+        self.s.close()
+
+
+    def close(self):
+        """
+        Close the UDP listener thread.
+        """
+        self.udp_listener_running = False
+        try:
+            self.t.join()
+        except:
+            pass
+
+
+    def handle_telemetry_packet(self, packet):
+        ''' Split a telemetry packet into time/lat/lon/alt, and pass it onto a callback '''
+
+        _fields = packet.split(',')
+        _short_time = _fields[1]
+        _lat = float(_fields[2])
+        _lon = float(_fields[3])
+        _alt = float(_fields[4])
+
+        # Timestamp Handling
+        # The 'short' timestamp (HH:MM:SS) is always assumed to be in UTC time.
+        # To build up a complete datetime object, we use the system's current UTC time, and replace the HH:MM:SS part.
+        _full_time = datetime.utcnow().strftime("%Y-%m-%dT") + _short_time + "Z"
+        _time_dt = parse(_full_time)
+
+        _output = {
+            'time'  : _time_dt,
+            'lat'   : _lat,
+            'lon'   : _lon,
+            'alt'   : _alt,
+            'comment' : 'Telemetry Data'
+        }
+
+        self.telemetry_callback(_output)
+
+
+    def handle_waypoint_packet(self, packet):
+        ''' Split a 'Waypoint' packet into fields, and pass onto a callback '''
+
+        _fields = packet.split(',')
+        _waypoint_name = _fields[1]
+        _lat = float(_fields[2])
+        _lon = float(_fields[3])
+        _comment = _fields[4]
+
+        _time_dt = datetime.utcnow()
+
+        _output = {
+            'time'  : _time_dt,
+            'name'  : _waypoint_name,
+            'lat'   : _lat,
+            'lon'   : _lon,
+            'comment' : _comment
+        }
+
+        self.waypoint_callback(_output)
+
+
+    def handle_packet(self, packet):
+        """
+        Check an incoming packet matches a valid type, and then forward it on.
+        """
+
+        # Extract header (first field)
+        packet_type = packet.split(',')[0]
+
+        if packet_type not in self.allowed_sentences:
+            print("ERROR: Got unknown packet: %s" % packet)
+            return
+
+        try:
+            # Now send on the packet if we are allowed to.
+            if packet_type == "TELEMETRY" and (self.telemetry_callback != None):
+                self.handle_telemetry_packet(packet)
+
+            # Generally we always want to pass on waypoint data.
+            if packet_type == "WAYPOINT" and (self.waypoint_callback != None):
+                self.send_packet_to_ozi(packet)
+
+        except:
+            print("ERROR: Error when handling packet.")
+
 
 if __name__ == '__main__':
     # Example script, essentially repeats functionality of PacketSniffer.py
