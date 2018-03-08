1b48ec520ca97cdeca906012c79558ad133e1844
Mark Jessop <vk5qi@rfhead.net>
2018-03-08 19:30:00 +1030

    Add header.

--- apps/kml_chase/kml_server.py
+++ apps/kml_chase/kml_server.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+#
+# Horus Ground Station - KML / GeoJSON Server
+#
+#   Copyright (C) 2018  Mark Jessop <vk5qi@rfhead.net>
+#   Released under GNU GPL v3 or later
+#
 
 import time, argparse, math, traceback, json
 from datetime import datetime
