1646c03936a876bf0f8adccfb1a2c213e6cc93b2
Mark Jessop <vk5qi@rfhead.net>
2018-03-09 17:07:42 +1030

    Added UDP broadcast handler example code.

---
+++ apps/other/udp_broadcast_handler_example.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Example Handling of UDP broadcast messages.
+# 2018-03-09 Mark Jessop <vk5qi@rfhead.net>
+#
+import time
+from horuslib.listener import *
+
+
+def handle_payload_summary(packet):
+    ''' Handle a 'Payload Summary' UDP broadcast message, supplied as a dict. '''
+    # Just print the packet.
+    print(packet)
+
+
+def handle_gps_data(packet):
+    ''' Handle a 'GPS' UDP broadcast message (sent by ChaseTracker), supplied as a dict. '''
+    # Just print the packet
+    print(packet)
+
+
+if __name__ == '__main__':
+    
+    # Instantiate the UDP listener.
+    udp_rx = UDPListener(
+        summary_callback = handle_payload_summary,
+        gps_callback = handle_gps_data
+        )
+    # and start it
+    udp_rx.start()
+
+    # From here, everything happens in the callback functions above.
+    try:
+        while True:
+            time.sleep(1)
+    # Catch CTRL+C nicely.
+    except KeyboardInterrupt:
+        # Close UDP listener.
+        udp_rx.close()
+        print("Closing.")
