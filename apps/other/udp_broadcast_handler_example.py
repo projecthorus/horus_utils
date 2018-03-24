5e0aeb3420bbb6ed6d7ccf4cc9e5011c05ef37e7
Mark Jessop <vk5qi@rfhead.net>
2018-03-24 22:40:43 +1030

    Add some better examples of packet processing into udp_broadcast_handler_example.py

--- apps/other/udp_broadcast_handler_example.py
+++ apps/other/udp_broadcast_handler_example.py
@@ -2,20 +2,57 @@
 # Example Handling of UDP broadcast messages.
 # 2018-03-09 Mark Jessop <vk5qi@rfhead.net>
 #
-import time
-from horuslib.listener import *
+import time, datetime
+from horuslib.listener import UDPListener
 
-
+# Exaple packet
+#{u'speed': -1, u'altitude': 12469, u'longitude': 139.23114, u'callsign': u'Radiosonde Auto RX', 
+# u'time': u'11:54:52', u'latitude': -34.99711, u'type': u'PAYLOAD_SUMMARY', u'heading': -1}
 def handle_payload_summary(packet):
     ''' Handle a 'Payload Summary' UDP broadcast message, supplied as a dict. '''
-    # Just print the packet.
-    print(packet)
+    # Extract fields
+    _callsign = packet['callsign']
+    _lat = packet['latitude']
+    _lon = packet['longitude']
+    _alt = packet['altitude']
+    _time = packet['time']
+    # There are also 'speed' and 'heading' fields, but currently nothing provides useful data in these.
+
+    # The comment field isn't always provided.
+    if 'comment' in packet:
+        _comment = packet['comment']
+    else:
+        _comment = "No Comment Provided"
+
+    # Print out the data as a string.
+    print("Payload Summary: %s, %s, %.5f, %.5f, %.1f, %s" % 
+        (_callsign,
+        _time,
+        _lat,
+        _lon,
+        _alt,
+        _comment
+        ))
 
 
 def handle_gps_data(packet):
     ''' Handle a 'GPS' UDP broadcast message (sent by ChaseTracker), supplied as a dict. '''
-    # Just print the packet
-    print(packet)
+    # Extract fields.
+    _lat = packet['latitude']
+    _lon = packet['longitude']
+    _alt = packet['altitude']
+    _speed = packet['speed']
+    # No timestamp is provided in these packets, so make our own.
+    _time = datetime.datetime.utcnow().isoformat()
+
+    # Print out the data as a string
+    print("Car GPS: %s, %.5f, %.5f, %.1f, %.1f" % 
+        (_time,
+        _lat,
+        _lon,
+        _alt,
+        _speed
+        ))
 
 
 def handle_all_packets(packet):
